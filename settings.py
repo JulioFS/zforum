b1584219745dc65e76c2de61b6d7775c472bd109
Julio F. Schwarzbeck <rustix.dev@gmail.com>
2024-02-26 18:56:16 -0800

    Updates settings to start on MacOS

--- settings.py
+++ settings.py
@@ -17,8 +17,8 @@ APP_NAME = os.path.split(APP_FOLDER)[-1]
 DB_FOLDER = required_folder(APP_FOLDER, 'databases')
 DB_URI = 'override-in-settings-private'
 DB_POOL_SIZE = 1
-DB_MIGRATE = False
-DB_FAKE_MIGRATE = True  # maybe?
+DB_MIGRATE = False # True if DB does not exist else False
+DB_FAKE_MIGRATE = True  # False id DB does not exist else True
 
 # location where static files are stored:
 STATIC_FOLDER = required_folder(APP_FOLDER, "static")
