0f9818a60118eed5b256892aaae83d452c548e5f
JulioFS <rustix.dev@gmail.com>
2024-03-15 15:01:40 -0700

    UI Modifications

--- controllers/channel.py
+++ controllers/channel.py
@@ -27,7 +27,7 @@ else your app will result in undefined behavior
 """
 
 import random
-from profanity import profanity
+from better_profanity import profanity
 from py4web import action, request, response, abort, redirect, URL
 from yatl.helpers import A
 from ..common import db, session, T, cache, auth, logger, authenticated, unauthenticated, groups
